7f90fe8bdc4d1a543e69b6a64fbe5c1cfb1a0226
mkhmmd17 <maxmelhi95@gmail.com>
2025-02-04 18:55:39 -0500

    Created a screens folder

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,11 @@
+import { TextInput, TextInputComponent, View } from "react-native";
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  );
+}
+
+export default StartGameScreen;
